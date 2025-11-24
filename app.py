b34f9a2c6a63b693949cd24448b043f7a2e49626
danielkonovak-cell <danielkonovak@gmail.com>
2025-11-24 13:43:09 +0100

    render_template

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 # ✅ import from the 'project' package
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # --- Main site pages (HTML templates) ---
 @app.get("/")
 def home():
-    return "Hello there"
-    #return render_template('lobby.html')
+    return render_template('lobby.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
