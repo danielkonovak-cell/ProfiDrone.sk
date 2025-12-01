ae99a90909c9a211e9d9a9d97fe4372ff1e9f335
danielkonovak-cell <danielkonovak@gmail.com>
2025-12-01 09:11:11 +0100

    ....

--- app.py
+++ app.py
@@ -1,28 +1,22 @@
-# ✅ import from the 'project' package
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# --- Main site pages (HTML templates) ---
-@app.get('/')
-def testing():
-    return render_template('index.html')
+@app.get("/")
+def index():
+    return render_template("index.html")
 
 @app.get("/farmers")
 def farmersPage():
-    return render_template('farmers.html')
-    #return render_template('lobby.html')
+    return render_template("farmers.html")
 
-@app.get("/sevice")
+@app.get("/service")
 def servicePage():
     return render_template("service.html")
 
+@app.get("/contact")
+def contactPage():
+    return render_template("contact.html")
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-
-
-# Too overloaded - menej contentu, zlednodusit!!!!
